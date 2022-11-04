fe7cf7e1bac08d85fc9da1a25317d2db39bf7f5a
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-11-04 19:30:58 -0300

    build: :wrench: config babel, jest and testing library

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  resetMocks: false,
+  moduleNameMapper: {
+    '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  testEnvironment: 'jsdom',
+  collectCoverage: false,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{tsx,ts}',
+    '!<rootDir>/**/*styles.ts',
+    '!<rootDir>/src/styles/**',
+    '!<rootDir>/src/App.tsx',
+    '!<rootDir>/src/main.tsx',
+    '!<rootDir>/src/tests/*.tsx',
+  ],
+  coveragePathIgnorePatterns: ['node_modules'],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageReporters: ['lcov', 'json'],
+};
