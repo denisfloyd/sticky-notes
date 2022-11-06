5c11b75ea0c3504befd095ae500ea64f0be6e3c1
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-11-06 17:58:13 -0300

    refactor: :truck: move AddStickyButton component

---
+++ src/features/sticky/components/AddStickyButton/AddStickyButton.unit.spec.tsx
@@ -0,0 +1,24 @@
+import { AddStickButton } from '.';
+import { fireEvent, render, screen } from '@/tests/test-utils';
+import { useSticky } from '@/features/sticky/contexts/StickyContext';
+
+const useStickyMock = useSticky as jest.Mock;
+jest.mock('@/features/sticky/contexts/StickyContext');
+
+describe('AddStickyButton', () => {
+  const addStickyMock = jest.fn();
+
+  beforeAll(() => {
+    useStickyMock.mockReturnValue({
+      addSticky: addStickyMock,
+    });
+  });
+
+  it('should be able to call addSticky', () => {
+    render(<AddStickButton />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(addStickyMock).toHaveBeenCalled();
+  });
+});
