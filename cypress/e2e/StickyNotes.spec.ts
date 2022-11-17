c3cc01164d84b0e923d6c50771512897b5f26e51
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-11-17 17:06:31 -0300

    test: :white_check_mark: add last ones e2e tests

--- cypress/e2e/StickyNotes.spec.ts
+++ cypress/e2e/StickyNotes.spec.ts
@@ -17,6 +17,10 @@ describe('StickyNotes Feature Page', () => {
       cy.get('header').should('exist');
       cy.get('textarea').should('exist');
     });
+
+    cy.then(() => {
+      cy.wrap(JSON.parse(localStorage.getItem('@sticky-notes/notes'))).should('not.be.empty');
+    });
   });
 
   it('should able to move a sticky note', () => {
@@ -32,9 +36,17 @@ describe('StickyNotes Feature Page', () => {
     cy.get("[data-testid='sticky']")
       .should('have.attr', 'style')
       .should('contain', 'transform', 'translate(300px, 180px)');
+
+    cy.wait(100);
+    cy.then(() => {
+      cy.wrap(JSON.parse(localStorage.getItem('@sticky-notes/notes')))
+        .should('not.be.empty')
+        .then((sticky) => Cypress._.map(sticky, (o) => Cypress._.pick(o, ['x', 'y'])))
+        .should('deep.include', { x: 300, y: 180 });
+    });
   });
 
-  it.only('should be able to remove a sticky note', () => {
+  it('should be able to remove a sticky note', () => {
     cy.contains('button', '+').click();
 
     cy.get("[data-testid='sticky']").within(() => {
@@ -45,5 +57,88 @@ describe('StickyNotes Feature Page', () => {
     });
 
     cy.get("[data-testid='sticky']").should('not.exist');
+
+    cy.then(() => {
+      cy.wrap(JSON.parse(localStorage.getItem('@sticky-notes/notes'))).should('be.empty');
+    });
+  });
+
+  it('should be able to edit a sticky note', () => {
+    cy.contains('button', '+').click();
+
+    cy.get("[data-testid='sticky']").within(() => {
+      cy.get('textarea').type('do something');
+    });
+
+    // debounce time
+    cy.wait(500);
+    cy.then(() => {
+      cy.wrap(JSON.parse(localStorage.getItem('@sticky-notes/notes')))
+        .should('not.be.empty')
+        .then((sticky) => Cypress._.map(sticky, (o) => Cypress._.pick(o, 'text')))
+        .should('deep.include', { text: 'do something' });
+    });
+  });
+
+  it('should be able to resize a sticky note', () => {
+    cy.contains('button', '+').click();
+
+    cy.get("[data-testid='sticky']").within(() => {
+      cy.get('textarea').invoke('attr', 'style', 'width: 300px; height: 300px');
+    });
+
+    cy.wait(100);
+    cy.then(() => {
+      cy.wrap(JSON.parse(localStorage.getItem('@sticky-notes/notes')))
+        .should('not.be.empty')
+        .then((sticky) => Cypress._.map(sticky, (o) => Cypress._.pick(o, ['width', 'height'])))
+        .should('deep.include', { width: 300, height: 300 });
+    });
+  });
+
+  it('should save stickies interations in localStorage and restore them after reload page', () => {
+    cy.contains('button', '+').click();
+    cy.contains('button', '+').click();
+
+    cy.get('#sticky-1').within(() => {
+      cy.get('header')
+        .trigger('mousedown', { which: 1, force: true })
+        .trigger('mousemove', { clientX: 200, clientY: 100, force: true })
+        .trigger('mouseup', { force: true });
+    });
+
+    cy.get('#sticky-1').within(() => {
+      cy.get('textarea').invoke('attr', 'style', 'width: 250px; height: 250px');
+    });
+
+    cy.get('#sticky-1').within(() => {
+      cy.get('textarea').type('do something #1', { force: true });
+    });
+
+    cy.get('#sticky-2').within(() => {
+      cy.get('header')
+        .trigger('mousedown', { which: 1, force: true })
+        .trigger('mousemove', { clientX: 750, clientY: 0, force: true })
+        .trigger('mouseup', { force: true });
+    });
+
+    cy.get('#sticky-2').within(() => {
+      cy.get('textarea').invoke('attr', 'style', 'width: 300px; height: 300px');
+    });
+
+    cy.get('#sticky-2').within(() => {
+      cy.get('textarea').type('do something #2', { force: true });
+    });
+
+    // debounce time
+    cy.wait(500);
+
+    cy.reload();
+
+    cy.get('#sticky-1').should('exist');
+    cy.get('#sticky-2').should('exist');
+
+    cy.get('#sticky-1').within(() => cy.get('textarea').should('contain', 'do something #1'));
+    cy.get('#sticky-2').within(() => cy.get('textarea').should('contain', 'do something #2'));
   });
 });
